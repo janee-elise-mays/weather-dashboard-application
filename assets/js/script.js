183f656fcc19f7091d9a8fa0e8c72d7ee14694f1
Janeé Mays <janee.elise@gmail.com>
2021-03-01 20:42:44 -0500

    added API key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,22 @@
-var fetchButton = document.getElementById('searchBtn');
+// var fetchButton = document.getElementById('searchBtn');
 
-function getApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}';
+// function getApi() {
+//     var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=54eeff228a31b67218a1e00174849d19={API key}';
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            for (var i = 0; i < data.length; i++) {
-                var listItem = document.createElement('li');
-                listItem.textContent = data[i].html_url;
-                repoList.appendChild(listItem);
-            }
-        });
-}
-fetchButton.addEventListener('click', getApi);
+//     fetch(requestUrl)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             for (var i = 0; i < data.length; i++) {
+//                 var listItem = document.createElement('li');
+//                 listItem.textContent = data[i].html_url;
+//                 repoList.appendChild(listItem);
+//             }
+//         });
+// }
+// fetchButton.addEventListener('click', getApi);
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={54eeff228a31b67218a1e00174849d19}')
+.then(response => response.json())
+.then(data => console.log(data));
