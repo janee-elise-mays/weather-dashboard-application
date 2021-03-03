bf87d8532a86c32c87488cd1ec1ebcd22f50758d
Janeé Mays <janee.elise@gmail.com>
2021-03-03 18:00:34 -0500

    revised js for city list. Still receiving Uncaught (in promise) TypeError: Failed to construct 'URL': Invalid URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,48 @@
-var fetchButton = document.getElementById('searchBtn');
-var searchInput = document.querySelector('.search-input');
-// function getApi() {
-//     var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=54eeff228a31b67218a1e00174849d19';
+var searchInput = document.getElementById('searchBtn');
 
-//     fetch(requestUrl)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             for (var i = 0; i < data.length; i++) {
-//                 var listItem = document.createElement('li');
-//                 listItem.textContent = data[i].html_url;
-//                 repoList.appendChild(listItem);
-//             }
-//         });
-// }
-console.log(fetchButton)
-fetchButton.addEventListener('click', function(event){
-    event.preventDefault()
-    fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=54eeff228a31b67218a1e00174849d19')
-    .then(response => response.json())
-    .then(data => console.log(data));
-});
+searchInput.addEventListener('click', function(event){
+    if(event.click == 'click'){
+        addItem();
+    }
+})
 
+function addItem(){
+    var city = document.getElementById('search-input').value;
+    var h1 = document.createElement('h1');
+
+    document.getElementById('search-input').value = '';
+    h1.appendChild(document.createTextNode(city));
+
+    var div = document.getElementById('city-searches');
+    div.insertAdjacentElement("beforeend", h1);
+}
+// var fetchButton = document.getElementById('searchBtn'); 
+function getApi() {
+    //     var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=54eeff228a31b67218a1e00174849d19';
+    
+    //     fetch(requestUrl)
+    //         .then(function (response) {
+    //             return response.json();
+    //         })
+    //         .then(function (data) {
+    //             for (var i = 0; i < data.length; i++) {
+    //                 var listItem = document.createElement('li');
+    //                 listItem.textContent = data[i].html_url;
+    //                 repoList.appendChild(listItem);
+    //             }
+    //         });
+    // }
+    // $('#searchBtn').click(function(){
+    //     var input = $(this).parent().attr('search-input');
+    //     var city = $(this).prev().val();
+    //     localStorage.setItem(input, city);
+    // })
+    
+    
+    // console.log(fetchButton)
+    // fetchButton.addEventListener('click', function(event){
+    //     event.preventDefault()
+    //     fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=54eeff228a31b67218a1e00174849d19')
+    //     .then(response => response.json())
+    //     .then(data => console.log(data));
+    // });
