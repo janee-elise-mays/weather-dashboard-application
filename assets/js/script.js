07c585f98bcbff53a399946c57a4984acd54d44e
Janeé Mays <janee.elise@gmail.com>
2021-03-04 13:06:13 -0500

    able to fix error by removing axe chrome extension

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,48 @@
 var searchInput = document.getElementById('searchBtn');
 
-searchInput.addEventListener('click', function(event){
-    if(event.click == 'click'){
-        addItem();
-    }
-})
+// searchInput.addEventListener('click', function (event) {
+//     if (event.click == 'click') {
+//         addItem();
+//     }
+// });
+// function addItem() {
+//     let city = document.getElementById('search-input').value;
+//     let h1 = document.createElement('h1');
 
-function addItem(){
-    var city = document.getElementById('search-input').value;
-    var h1 = document.createElement('h1');
+//     document.getElementById('search-input').value = '';
+    
+//     h1.appendChild(document.createTextNode(city));
 
-    document.getElementById('search-input').value = '';
-    h1.appendChild(document.createTextNode(city));
+//     var div = document.getElementById('city-searches');
+//     div.insertAdjacentElement("beforeend", h1);
+// }
 
-    var div = document.getElementById('city-searches');
-    div.insertAdjacentElement("beforeend", h1);
-}
-// var fetchButton = document.getElementById('searchBtn'); 
-function getApi() {
-    //     var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=54eeff228a31b67218a1e00174849d19';
-    
-    //     fetch(requestUrl)
-    //         .then(function (response) {
-    //             return response.json();
-    //         })
-    //         .then(function (data) {
-    //             for (var i = 0; i < data.length; i++) {
-    //                 var listItem = document.createElement('li');
-    //                 listItem.textContent = data[i].html_url;
-    //                 repoList.appendChild(listItem);
-    //             }
-    //         });
-    // }
-    // $('#searchBtn').click(function(){
-    //     var input = $(this).parent().attr('search-input');
-    //     var city = $(this).prev().val();
-    //     localStorage.setItem(input, city);
-    // })
-    
-    
-    // console.log(fetchButton)
-    // fetchButton.addEventListener('click', function(event){
-    //     event.preventDefault()
-    //     fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=54eeff228a31b67218a1e00174849d19')
-    //     .then(response => response.json())
-    //     .then(data => console.log(data));
-    // });
+// function getApi() {
+//         var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=54eeff228a31b67218a1e00174849d19';
+
+//         fetch(requestUrl)
+//             .then(function (response) {
+//                 return response.json();
+//             })
+//             .then(function (data) {
+//                 for (var i = 0; i < data.length; i++) {
+//                     var listItem = document.createElement('li');
+//                     listItem.textContent = data[i].html_url;
+//                     repoList.appendChild(listItem);
+//                 }
+//             });
+//     }
+//     $('searchBtn').click(function addItem(){
+//         var input = $(this).parent().attr('search-input');
+//         var city = $(this).prev().val();
+//         localStorage.setItem(input, city);
+//     })
+
+
+    // console.log(searchBtn)
+    searchBtn.addEventListener('click', function (event) {
+        event.preventDefault()
+        fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=54eeff228a31b67218a1e00174849d19')
+            .then(response => response.json())
+            .then(data => console.log(data));
+    })
