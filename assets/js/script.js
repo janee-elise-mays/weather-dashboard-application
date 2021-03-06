6216964dbd2e4fd1264b3aa0301cffa05a2b76c3
Janeé Mays <janee.elise@gmail.com>
2021-03-06 16:23:20 -0500

    updated js to pull data from api with 5 day weather forecast as well as correct temp metric

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,16 @@
-var searchInput = document.getElementById('searchBtn');
+var button = document.querySelector('.searchBtn')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var date = document.querySelector('.date');
+var weather = document.querySelector('.weather-conditions');
+var temp = document.querySelector('.temp');
+var humidity = document.querySelector('.humidity');
+var wind = document.querySelector('.wind-speed');
+var index = document.querySelector('.uv-index');
 
-// searchInput.addEventListener('click', function (event) {
-//     if (event.click == 'click') {
-//         addItem();
-//     }
-// });
-// function addItem() {
-//     let city = document.getElementById('search-input').value;
-//     let h1 = document.createElement('h1');
-
-//     document.getElementById('search-input').value = '';
-    
-//     h1.appendChild(document.createTextNode(city));
-
-//     var div = document.getElementById('city-searches');
-//     div.insertAdjacentElement("beforeend", h1);
-// }
-
-// function getApi() {
-//         var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=54eeff228a31b67218a1e00174849d19';
-
-//         fetch(requestUrl)
-//             .then(function (response) {
-//                 return response.json();
-//             })
-//             .then(function (data) {
-//                 for (var i = 0; i < data.length; i++) {
-//                     var listItem = document.createElement('li');
-//                     listItem.textContent = data[i].html_url;
-//                     repoList.appendChild(listItem);
-//                 }
-//             });
-//     }
-//     $('searchBtn').click(function addItem(){
-//         var input = $(this).parent().attr('search-input');
-//         var city = $(this).prev().val();
-//         localStorage.setItem(input, city);
-//     })
-
-
-    // console.log(searchBtn)
-    searchBtn.addEventListener('click', function (event) {
-        event.preventDefault()
-        fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=54eeff228a31b67218a1e00174849d19')
-            .then(response => response.json())
-            .then(data => console.log(data));
-    })
+button.addEventListener('click', function() {
+event.preventDefault()
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+ inputValue.value +'&cnt=5&units=imperial&appid=54eeff228a31b67218a1e00174849d19')
+    .then(response => response.json())
+    .then(data => console.log(data))
+});
