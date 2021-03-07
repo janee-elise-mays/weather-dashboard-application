94cde678add22853a2bcbc18f2c51203a968319b
Janeé Mays <janee.elise@gmail.com>
2021-03-06 22:08:59 -0500

    added brief readme content and css

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ A weather dashboard that will run in the browser and feature dynamically updated
 
 # Description
 
+I really struggled with this exercise. I was able to pull the needed array from the api. Where I hit a major hurdle was displaying the desired weather conditions. Also, again I struggled a lot with local storage. 
+
 https://janee-elise-mays/weather-dashboard-application
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,24 +7,25 @@ var temp = document.querySelector('.temp');
 var humidity = document.querySelector('.humidity');
 var wind = document.querySelector('.wind-speed');
 var index = document.querySelector('.uv-index');
+var citySearch = document.querySelector('#city-search');
 
-button.addEventListener('click', function() {
-event.preventDefault()
-fetch('https://api.openweathermap.org/data/2.5/forecast?q='+ inputValue.value +'&cnt=5&units=imperial&appid=54eeff228a31b67218a1e00174849d19')
-    .then(response => response.json())
-    .then(data => {
-        var nameValue = data['city']['name'];
-        // var weatherValue = data['list']['0']['weather']['0']['description'];
-        var tempValue = data['list']['0']['main']['temp'];
-        var humidityValue = data['list']['0']['main']['humidity'];
-        // var windValue = data['list']['0']['wind']['speed'];
-        document.querySelector('.name').innerHTML = nameValue;
-        // document.querySelector('.weather').innerHTML = weatherValue;
-        document.querySelector('.temp').innerHTML = tempValue;
-        document.querySelector('.humidity').innerHTML = humidityValue;
-        // document.querySelector('.wind').innerHTML = windValue;
-        
-    })
+button.addEventListener('click', function () {
+    event.preventDefault()
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + inputValue.value + '&cnt=5&units=imperial&appid=54eeff228a31b67218a1e00174849d19')
+        .then(response => response.json())
+        .then(data => {
+            var nameValue = data['city']['name'];
+            // var weatherValue = data['list']['0']['weather']['0']['description'];
+            var tempValue = data['list']['0']['main']['temp'];
+            var humidityValue = data['list']['0']['main']['humidity'];
+            // var windValue = data['list']['0']['wind']['speed'];
+            document.querySelector('.name').innerHTML = nameValue;
+            // document.querySelector('.weather').innerHTML = weatherValue;
+            document.querySelector('.temp').innerHTML = tempValue;
+            document.querySelector('.humidity').innerHTML = humidityValue;
+            // document.querySelector('.wind').innerHTML = windValue;
+
+        })
 });
 
 // button.addEventListener('click',function () {
@@ -35,8 +36,31 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+ inputValue.value +'
 
 //     // var = [" "]
 // });
-button.addEventListener('click',function () {
-        var search = $(this).parent().attr('search-box');
-        var city = $(this).prev().val();
-        localStorage.setItem(search, city);
+
+let userSearch;
+let cityList;
+
+const itemArr = JSON.parse(localStorage.getItem('inputValue')) || [];
+
+button.addEventListener('click', function () {
+    event.preventDefault();
+    userSearch = inputValue.value;
+    itemArr.push(userSearch);
+    localStorage.setItem('inputValue', JSON.stringify(itemArr));
+    itemArr.forEach(search => {
+        cityList = document.createElement('div');
+        citySearch.appendChild(cityList);
+        let cityText = document.createElement('p');
+        cityText.innerHTML=citySearch;
+        cityList.className="city-list";
+        cityList.appendChild(cityText);
+        button.addEventListener('click', function () {
+        cityList.remove();
+        })
     })
+});
+button.addEventListener('click', function() {
+    inputValue.innerHTML = '';
+});
+
+
